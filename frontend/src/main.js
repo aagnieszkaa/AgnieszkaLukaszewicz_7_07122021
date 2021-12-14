fd86576cadde8d3abe817b72b3d40ff26301fb74
Agnieszka Lukaszewicz <a.lukaszewicz@outlook.com>
2021-12-14 12:26:27 +0100

    bootstrap pb2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import BootstrapVue from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-createApp(App).use(store).use(router).use(BootstrapVue).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
 
