ddfc9021ad859c3d1644fd402b149fe3393f58c8
Agnieszka Lukaszewicz <a.lukaszewicz@outlook.com>
2022-01-15 04:40:15 +0100

    header OK

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -15,13 +15,10 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-$color-primary: #2c9586;
-
   .header{
     padding: 10px 0;
-    background-color: $color-primary;
+    background-color: $color-strong;
     width: 100%;
   }
 </style>
