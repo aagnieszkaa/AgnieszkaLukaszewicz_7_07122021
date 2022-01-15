48fae55dbebe4349ac4e83706c2d2268b8d60654
Agnieszka Lukaszewicz <a.lukaszewicz@outlook.com>
2022-01-15 06:00:14 +0100

    signup OK

--- frontend/src/views/Profil.vue
+++ frontend/src/views/Profil.vue
@@ -3,61 +3,53 @@
     <Header></Header>
     <Menu></Menu>
     <div class="container">
-
-        <b-container class="my-4">
-            <b-row>
-                <b-card
-                no-body
-                class="profil-card 
-                mb-2 
-                offset-1 col-10
-                offset-md-3 col-md-6
-                offset-lg-4 col-lg-4
-                 ">
-                      <b-card-img 
-                      :src="utilisateur.image_chemin" 
-                      alt="Photo de profil" 
-                      top 
-                      class="mb-2 profil-card__photo">
-                      </b-card-img>
-                    <b-card-body class="d-flex flex-column justify-content-center align-items-center">
-
-                      <b-card-title>{{utilisateur.prenom}} {{utilisateur.nom}}</b-card-title>
-                      <b-card-text v-if="chargeCommunication">Chargé de la communication</b-card-text>
-                      <b-card-text v-else>Employée</b-card-text>
-                      <b-card-text>{{utilisateur.email}}</b-card-text>
-                          <b-button 
-                          v-if="this.utilisateur_token_id.utilisateurId == this.urlId"
-                          variant="primary"
-                          class="col-8"
-                          @click="editUser()"
-                          >Modifier le compte</b-button><br>
-
-                          <b-button 
-                          variant="primary"
-                          v-if="this.utilisateur_token_id.utilisateurId == this.urlId"
-                          class="col-8"
-                          @click="deleteUser()"
-                          >Supprimer le compte</b-button><br>
-
-
-
-                          <b-button 
-                          variant="secondary"
-                          v-if="this.utilisateur_token_id.utilisateurId == this.urlId"
-                          class="col-8 mt-4"
-                          @click="logout()"
-                          >Déconnexion</b-button>
-                    </b-card-body>
-                </b-card>
-            </b-row>
-        </b-container>
+      <b-container class="my-4">
+        <b-row>
+          <b-card
+          no-body
+          class="profil-card 
+          mb-2 
+          offset-1 col-10
+          offset-md-3 col-md-6
+          offset-lg-4 col-lg-4">
+            <b-card-img 
+            :src="utilisateur.image_chemin" 
+            alt="Photo de profil" 
+            top 
+            class="mb-2 profil-card__photo">
+            </b-card-img>
+            <b-card-body class="d-flex flex-column justify-content-center align-items-center">
+              <b-card-title>{{utilisateur.prenom}} {{utilisateur.nom}}</b-card-title>
+              <b-card-text v-if="chargeCommunication">Chargé de la communication</b-card-text>
+              <b-card-text v-else>Employée</b-card-text>
+              <b-card-text>{{utilisateur.email}}</b-card-text>
+              <b-button 
+              v-if="this.utilisateur_token_id.utilisateurId == this.urlId"
+              variant="primary"
+              class="col-8"
+              @click="editUser()"
+              >Modifier le compte</b-button><br>
+              <b-button 
+              variant="primary"
+              v-if="this.utilisateur_token_id.utilisateurId == this.urlId"
+              class="col-8"
+              @click="deleteUser()"
+              >Supprimer le compte</b-button><br>
+              <b-button 
+              variant="secondary"
+              v-if="this.utilisateur_token_id.utilisateurId == this.urlId"
+              class="col-8 mt-4"
+              @click="logout()"
+              >Déconnexion</b-button>
+            </b-card-body>
+          </b-card>
+        </b-row>
+      </b-container>
     </div> 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Menu from '@/components/Menu.vue'
 import { mapState } from 'vuex';
@@ -70,7 +62,7 @@ export default {
   },
   data: function () {
     return {
-        urlId: this.$route.params.id,
+      urlId: this.$route.params.id,
     }
   },
   mounted: function (){
@@ -84,14 +76,14 @@ export default {
   computed: {
     chargeCommunication: function () {
       if (this.utilisateur.fonction == false) {
-          return false;
+        return false;
       } else {
-          return true;
+        return true;
       }
     },
     ...mapState({
-      utilisateur: 'utilisateurInfo',
-      utilisateur_token_id: 'utilisateur',
+    utilisateur: 'utilisateurInfo',
+    utilisateur_token_id: 'utilisateur',
     })
   },
   methods: {
@@ -114,7 +106,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 .profil-card {
   overflow: hidden;
   border-radius: 2%;
@@ -126,6 +117,4 @@ export default {
     object-fit: cover;
   }
 }
-
-
 </style>
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,166 +1,153 @@
 <template>
-    <div>
-        <Header></Header>
-        <b-container class="mt-4 mb-4">
-            <b-row class="form">
-                <b-form class="
-                offset-1 col-10 
-                offset-md-2 col-md-8
-                offset-lg-4 col-lg-4
-                formulaire bg-light">
-                    <h2 v-if="mode == 'signup'">Créer votre compte</h2>
-                    <h2 v-else>Connectez-vous</h2>
-                    <p v-if="mode == 'signup'">Vous avez déjà le compte ? <span class="link" @click="switchToConnexion()">Connectez-vous</span></p>
-                    <p v-else>Vous n'avez pas de compte ? <span class="link" @click="switchToCreation()">Créer votre compte</span></p>
-                    <b-form-group
-                    label="Nom :"
-                    label-for="input-nom"
-                    class="mb-2 mt-4"
-                    v-if="mode == 'signup'">
-                        <b-form-input
-                        id="input-nom"
-                        v-model="state.input.nom"
-                        type="text">
-                        </b-form-input>
-                        <span class="error" v-if="vSignup$.input.nom.$error">
-                          {{ vSignup$.input.nom.$errors[0].$message }}
-                        </span>
-                    </b-form-group>
+  <div>
+    <Header></Header>
+    <b-container class="mt-4 mb-4">
+      <b-row class="form">
+        <b-form class="
+        offset-1 col-10 
+        offset-md-2 col-md-8
+        offset-lg-4 col-lg-4
+        formulaire bg-light">
+          <h2 v-if="mode == 'signup'">Créer votre compte</h2>
+          <h2 v-else>Connectez-vous</h2>
+          <p v-if="mode == 'signup'">Vous avez déjà le compte ? <span class="formulaire__link" @click="switchToConnexion()">Connectez-vous</span></p>
+          <p v-else>Vous n'avez pas de compte ? <span class="formulaire__link" @click="switchToCreation()">Créer votre compte</span></p>
+          <b-form-group
+          label="Nom :"
+          label-for="input-nom"
+          class="mb-2 mt-4"
+          v-if="mode == 'signup'">
+            <b-form-input
+            id="input-nom"
+            v-model="state.input.nom"
+            type="text">
+            </b-form-input>
+            <span class="error" v-if="vSignup$.input.nom.$error">
+              {{ vSignup$.input.nom.$errors[0].$message }}
+            </span>
+          </b-form-group>
+          <b-form-group
+          label="Prénom :"
+          label-for="input-prenom"
+          class="mb-2"
+          v-if="mode == 'signup'">
+            <b-form-input
+            id="input-prenom"
+            v-model="state.input.prenom"
+            type="text">
+            </b-form-input>
+            <span class="error" v-if="vSignup$.input.prenom.$error">
+              {{ vSignup$.input.prenom.$errors[0].$message }}
+            </span>
+          </b-form-group>
+          <b-form-group
+          label="E-mail :"
+          label-for="input-email"
+          class="mb-2">
+            <b-form-input
+            id="input-email"
+            v-model="state.input.email"
+            type="email">
+            </b-form-input>
+            <div v-if="mode == 'signup'">
+              <span class="error" v-if="vSignup$.input.email.$error">
+                {{ vSignup$.input.email.$errors[0].$message }}
+              </span>
+            </div>
+            <div v-else>
+              <span class="error" v-if="vLogin$.input.email.$error">
+                {{ vLogin$.input.email.$errors[0].$message }}
+              </span>
+            </div>
+          </b-form-group>
+          <b-form-group
+          label="Mot de passe :"
+          label-for="input-mot_de_passe"
+          class="mb-2">
+            <b-form-input
+            id="input-mot_de_passe"
+            v-model="state.input.mot_de_passe"
+            type="password">
+            </b-form-input>
+            <div v-if="mode == 'signup'">
+              <span class="error" v-if="vSignup$.input.mot_de_passe.$error">
+                {{ vSignup$.input.mot_de_passe.$errors[0].$message }}
+              </span>
+            </div>
+            <div v-else>
+              <span class="error" v-if="vLogin$.input.mot_de_passe.$error">
+                {{ vLogin$.input.mot_de_passe.$errors[0].$message }}
+              </span>
+            </div>
+          </b-form-group>
+          <b-form-group
+          label="Photo :"
+          label-for="input-photo"
+          class="mb-2"
+          v-if="mode == 'signup'">
+            <input type="file" 
+            id="input-photo"
+            class="formulaire__input--photo"
+            accept="image/*"
+            @change="photoChange">
+            <span class="error" v-if="vSignup$.profil_image.$error">
+              {{ vSignup$.profil_image.$errors[0].$message }}
+            </span>
+          </b-form-group>
+          <b-form-group
+          label="Fonction :"
+          class="mb-2"
+          v-if="mode == 'signup'">
+            <b-form-radio 
+            name="input-radio" 
+            value="1"
+            v-model="state.input.fonction">
+            Chargé de la communication
+            </b-form-radio>
 
-                    <b-form-group
-                    label="Prénom :"
-                    label-for="input-prenom"
-                    class="mb-2"
-                    v-if="mode == 'signup'">
-                        <b-form-input
-                        id="input-prenom"
-                        v-model="state.input.prenom"
-                        type="text">
-                        </b-form-input>
-                        <span class="error" v-if="vSignup$.input.prenom.$error">
-                          {{ vSignup$.input.prenom.$errors[0].$message }}
-                        </span>
-                    </b-form-group>
-
-                    <b-form-group
-                    label="E-mail :"
-                    label-for="input-email"
-                    class="mb-2">
-                        <b-form-input
-                        id="input-email"
-                        v-model="state.input.email"
-                        type="email">
-                        </b-form-input>
-                        <div v-if="mode == 'signup'">
-                          <span class="error" v-if="vSignup$.input.email.$error">
-                            {{ vSignup$.input.email.$errors[0].$message }}
-                          </span>
-                        </div>
-                        <div v-else>
-                          <span class="error" v-if="vLogin$.input.email.$error">
-                            {{ vLogin$.input.email.$errors[0].$message }}
-                          </span>
-                          
-                        </div>
-                    </b-form-group>
-
-                    <b-form-group
-                    label="Mot de passe :"
-                    label-for="input-mot_de_passe"
-                    class="mb-2">
-                        <b-form-input
-                        id="input-mot_de_passe"
-                        v-model="state.input.mot_de_passe"
-                        type="password">
-                        </b-form-input>
-                        <div v-if="mode == 'signup'">
-                          <span class="error" v-if="vSignup$.input.mot_de_passe.$error">
-                            {{ vSignup$.input.mot_de_passe.$errors[0].$message }}
-                          </span>
-                        </div>
-                        <div v-else>
-                          <span class="error" v-if="vLogin$.input.mot_de_passe.$error">
-                            {{ vLogin$.input.mot_de_passe.$errors[0].$message }}
-                          </span>
-                        </div>
-                    </b-form-group>
-
-                    <b-form-group
-                    label="Photo :"
-                    label-for="input-photo"
-                    class="mb-2"
-                    v-if="mode == 'signup'">
-                        <input type="file" 
-                        id="input-photo"
-                        class="input--photo"
-                        accept="image/*"
-                        @change="photoChange">
-                        <span class="error" v-if="vSignup$.profil_image.$error">
-                          {{ vSignup$.profil_image.$errors[0].$message }}
-                        </span>
-                    </b-form-group>
-
-                    <b-form-group
-                    label="Fonction :"
-                    class="mb-2"
-                    v-if="mode == 'signup'">
-                        <b-form-radio 
-                        name="input-radio" 
-                        value="1"
-                        v-model="state.input.fonction">
-                        Chargé de la communication
-                        </b-form-radio>
-
-                        <b-form-radio 
-                        name="input-radio" 
-                        value="0"
-                        v-model="state.input.fonction">
-                        Employée
-                        </b-form-radio>
-                      <span class="error" v-if="vSignup$.input.fonction.$error">
-                          {{ vSignup$.input.fonction.$errors[0].$message }}
-                      </span>
-                    </b-form-group>
-                    
-                    <b-form-group
-                    label="Mot de passe fourni par les RH :"
-                    label-for="input-mot_de_passe_RH"
-                    class="mb-2"
-                    v-if="mode == 'signup' && optionCommunication">
-                        <b-form-input
-                        id="input-mot_de_passe_RH"
-                        v-model="state.input.mot_de_passe_RH"
-                        type="password">
-                        </b-form-input>
-                        <span class="error" v-if="vSignup$.input.mot_de_passe_RH.$error">
-                          {{ vSignup$.input.mot_de_passe_RH.$errors[0].$message }}
-                      </span>
-                    </b-form-group>
-
-                    <b-button 
-                    variant="primary"
-                    v-if="mode == 'signup'"
-                    @click="signup">Créez votre compte</b-button>
-
-                    <b-button 
-                    variant="primary"
-                    v-else
-                    @click="login()">Connectez-vous</b-button><br>  
-                    <span class="error">
-                      {{ error }}
-                    </span>
-                </b-form>
-            </b-row>
-
-        </b-container>
-        
-        
-        <!--<span v-if="state == 'connexionEnCours'">Connexion en cours</span>-->
-    </div>
+            <b-form-radio 
+            name="input-radio" 
+            value="0"
+            v-model="state.input.fonction">
+            Employée
+            </b-form-radio>
+            <span class="error" v-if="vSignup$.input.fonction.$error">
+                {{ vSignup$.input.fonction.$errors[0].$message }}
+            </span>
+          </b-form-group>
+          <b-form-group
+          label="Mot de passe fourni par les RH :"
+          label-for="input-mot_de_passe_RH"
+          class="mb-2"
+          v-if="mode == 'signup' && optionCommunication">
+            <b-form-input
+            id="input-mot_de_passe_RH"
+            v-model="state.input.mot_de_passe_RH"
+            type="password">
+            </b-form-input>
+            <span class="error" v-if="vSignup$.input.mot_de_passe_RH.$error">
+              {{ vSignup$.input.mot_de_passe_RH.$errors[0].$message }}
+            </span>
+          </b-form-group>
+          <b-button 
+          variant="primary"
+          v-if="mode == 'signup'"
+          @click="signup">Créez votre compte</b-button>
+          <b-button 
+          variant="primary"
+          v-else
+          @click="login()">Connectez-vous</b-button><br>  
+          <span class="error">
+            {{ error }}
+          </span>
+        </b-form>
+      </b-row>
+    </b-container>  
+      <!--<span v-if="state == 'connexionEnCours'">Connexion en cours</span>-->
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import { mapState } from 'vuex';
 import Header from '@/components/Header.vue'
 import useVuelidate from '@vuelidate/core'
@@ -172,7 +159,7 @@ export default {
   components: {
     Header
   },
-setup () {
+  setup () {
     const state = reactive({
       input: {
         nom: '',
@@ -184,8 +171,7 @@ setup () {
       },
       profil_image: null
     })
-
-const rulesSignup = computed(() => {
+    const rulesSignup = computed(() => {
       return {
         input: {
           nom: { required: helpers.withMessage('Veuillez renseigner ce champ !', required) },
@@ -198,24 +184,22 @@ const rulesSignup = computed(() => {
         profil_image: { required: helpers.withMessage('Veuillez renseigner ce champ !', required) }
       }
     })
-
-const rulesLogin = computed(() => {
+    const rulesLogin = computed(() => {
       return {
         input: {
           email: { required: helpers.withMessage('Veuillez renseigner ce champ !', required), email: helpers.withMessage('Veuillez saisir une adresse mail valide !', email) },
           mot_de_passe: { required: helpers.withMessage('Veuillez renseigner ce champ !', required) },
-      }}
+        }
+      }
     })
-
     const vSignup$ = useVuelidate(rulesSignup, state)
     const vLogin$ = useVuelidate(rulesLogin, state)
-
     return { state, vSignup$, vLogin$ }
   },
   data: function () {
     return {
-        mode: 'signup',
-        error: '',
+      mode: 'signup',
+      error: '',
     }
   },
   mounted: function () {
@@ -224,66 +208,65 @@ const rulesLogin = computed(() => {
       return ;
     }
   },
-computed: {
+  computed: {
     optionCommunication: function () {
       const value = this.state.input.fonction;
       if (this.mode == 'signup' && value == 0) {
-          return false;
+        return false;
       } else {
-          return true;
+        return true;
       }
     },
-    //...mapState(['status'])  
     ...mapState({
-        utilisateurInfo: 'utilisateurInfo',
-        utilisateur_token_id: 'utilisateur',
-        status: 'status'
+    utilisateurInfo: 'utilisateurInfo',
+    utilisateur_token_id: 'utilisateur',
+    status: 'status'
     }) 
   },
   methods: {
-      switchToConnexion: function () {
-          this.error = '';
-          this.mode = 'login';
-      },
-      switchToCreation: function () {
-          this.error = '';
-          this.mode = 'signup';   
-      },
-      photoChange: function (event) {
-          this.state.profil_image = event.target.files[0];
-      },
-      submitFormSignup() {
-        this.vSignup$.$validate();
-        if(!this.vSignup$.$error) {
-          return true;
-        } else {
-          this.error = '';
-          return false;
-        }
-      },
-      submitFormLogin() {
-        this.vLogin$.$validate();
-        if(!this.vLogin$.$error) {
-          return true;
-        } else {
-          this.error = '';
-          return false;
-        }
-      },
-      signup: function () {
-        if(this.submitFormSignup()) {
-          const self = this;
-          const fd = new FormData();
-          fd.append('profil_image', this.state.profil_image);
-          fd.append('utilisateur', JSON.stringify(this.state.input));
-          this.$store.dispatch('signup', fd)
-          .then(function () {
-            self.login();
-          }, function (error) {
-            self.error = error.response.data.error;
-          })
-        }
-      },
+    switchToConnexion: function () {
+      this.error = '';
+      this.mode = 'login';
+    },
+    switchToCreation: function () {
+      this.error = '';
+      this.mode = 'signup';   
+    },
+    photoChange: function (event) {
+      this.state.profil_image = event.target.files[0];
+    },
+    submitFormSignup() {
+      this.vSignup$.$validate();
+      if(!this.vSignup$.$error) {
+        return true;
+      } else {
+        this.error = '';
+        return false;
+      }
+    },
+    submitFormLogin() {
+      this.vLogin$.$validate();
+      if(!this.vLogin$.$error) {
+        return true;
+      } else {
+        this.error = '';
+        return false;
+      }
+    },
+    signup: function () {
+      if(this.submitFormSignup()) {
+        const self = this;
+        const fd = new FormData();
+        fd.append('profil_image', this.state.profil_image);
+        fd.append('utilisateur', JSON.stringify(this.state.input));
+        this.$store.dispatch('signup', fd)
+        .then(function () {
+          self.login();
+        }, function (error) {
+          self.error = error.response.data.error;
+        })
+      }
+    },
     login: function () {
       if(this.submitFormLogin()) {
         const self = this;
@@ -305,17 +288,16 @@ computed: {
 .formulaire  {
   padding: 10px;
   border-radius: 2%;
-  border: 1px solid #bdc7d0;
-}
-.link
-{
+  @include border;
+  &__link {
     color: blue;
+  }
+  &__input--photo {
+  width: 100%;
+  }
 }
 input {
   width: 70%;
-}
-.input--photo {
-  width: 100%;
 }
 </style>
 
