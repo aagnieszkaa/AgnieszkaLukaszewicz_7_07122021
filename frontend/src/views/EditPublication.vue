1bcb01c9dfe55c7e44a425f5471904f742021639
Agnieszka Lukaszewicz <a.lukaszewicz@outlook.com>
2022-01-15 05:17:14 +0100

    editPublication OK

--- frontend/src/views/EditPublication.vue
+++ frontend/src/views/EditPublication.vue
@@ -1,89 +1,78 @@
 <template>
-    <div>
-        <Header></Header>
-        <Menu></Menu>
-        <b-container class="mt-4">
-            <b-row>
-                <b-form class="bg-light my-4 p-4 formulaire">
-                    <h2>Modifiez votre publication</h2>
-
-                    <b-form-group
-                    label="Titre :"
-                    label-for="input-title"
-                    class="mb-2">
-                        <b-form-input
-                        id="input-title"
-                        v-model="state.input.title"
-                        type="text">
-                        </b-form-input>
-                        <span class="error" v-if="vModification$.input.title.$error">
-                            {{ vModification$.input.title.$errors[0].$message }}
-                        </span>
-                    </b-form-group>
-
-                    <b-form-group
-                    label="Description :"
-                    label-for="input-description"
-                    class="mb-2">
-                        <b-form-textarea
-                        id="input-description"
-                        v-model="state.input.textContent"
-                        rows="3"
-                        max-rows="6"
-                        ></b-form-textarea>
-                        <span class="error" v-if="vModification$.input.textContent.$error">
-                            {{ vModification$.input.textContent.$errors[0].$message }}
-                        </span>
-                    </b-form-group>
-
-                    <b-row>
-                        <img
-                        class="col-5"
-                        ref="photoMeme"
-                        :src="publicationInfo.post_image" />
-                        <img
-                        class="col-5"
-                        ref="preview"
-                        src="" />
-                    </b-row>
-
-                    <b-form-group
-                    label="Image :"
-                    label-for="input-image"
-                    class="mb-2">
-                        <input type="file" 
-                        id="input-image"
-                        accept="image/*"
-                        @change="photoChange">
-                    </b-form-group>
-
-                    <b-button 
-                    variant="primary"
-                    class="col-5 
-
-                    mt-3"
-                    @click="editPost()">
-                    Enregistrer</b-button>
-
-                    <b-button 
-                    variant="primary"
-                    class="offset-2 col-5 
-
-                    mt-3"
-                    @click="abandon()">Abandonner</b-button>
-
-                </b-form>
-            </b-row>
-        </b-container>
-    </div>
+  <div>
+    <Header></Header>
+    <Menu></Menu>
+    <b-container class="mt-4">
+      <b-row>
+        <b-form class="bg-light my-4 p-4 formulaire">
+          <h2>Modifiez votre publication</h2>
+          <b-form-group
+          label="Titre :"
+          label-for="input-title"
+          class="mb-2">
+            <b-form-input
+            id="input-title"
+            v-model="state.input.title"
+            type="text">
+            </b-form-input>
+            <span class="error" v-if="vModification$.input.title.$error">
+                {{ vModification$.input.title.$errors[0].$message }}
+            </span>
+          </b-form-group>
+          <b-form-group
+          label="Description :"
+          label-for="input-description"
+          class="mb-2">
+            <b-form-textarea
+            id="input-description"
+            v-model="state.input.textContent"
+            rows="3"
+            max-rows="6"
+            ></b-form-textarea>
+            <span class="error" v-if="vModification$.input.textContent.$error">
+                {{ vModification$.input.textContent.$errors[0].$message }}
+            </span>
+          </b-form-group>
+          <b-row>
+            <img
+            class="col-5"
+            ref="photoMeme"
+            :src="publicationInfo.post_image" />
+            <img
+            class="col-5"
+            ref="preview"
+            src="" />
+          </b-row>
+          <b-form-group
+          label="Image :"
+          label-for="input-image"
+          class="mb-2">
+              <input type="file" 
+              id="input-image"
+              accept="image/*"
+              @change="photoChange">
+          </b-form-group>
+          <b-button 
+          variant="primary"
+          class="col-5 
+          mt-3"
+          @click="editPost()">
+          Enregistrer</b-button>
+          <b-button 
+          variant="primary"
+          class="offset-2 col-5 
+          mt-3"
+          @click="abandon()">Abandonner</b-button>
+        </b-form>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-
 import { mapState } from 'vuex';
 import Header from '@/components/Header.vue'
 import Menu from '@/components/Menu.vue'
-
 import useVuelidate from '@vuelidate/core'
 import { required, helpers } from '@vuelidate/validators'
 import { reactive, computed } from 'vue'
@@ -96,12 +85,11 @@ export default {
   },
   computed: {
     ...mapState({
-      publications: 'publications',
-      publicationInfo: 'publicationInfo',
+    publications: 'publications',
+    publicationInfo: 'publicationInfo',
     })
   },
-  
-  mounted: function (){
+  mounted: function () {
     this.$refs.preview.style.display = "none";
     const self = this;
     const urlId = this.$route.params.postId;
@@ -123,37 +111,37 @@ export default {
         input: {
           title: { required: helpers.withMessage('Veuillez renseigner ce champ !', required) },
           textContent: { required: helpers.withMessage('Veuillez renseigner ce champ !', required) },
-      }}
+        }
+      }
     })
     const vModification$ = useVuelidate(rulesModification, state)
     return { state, vModification$ }
   },
   data: function () {
     return {
-        error: '',
-        post_image: null
+      error: '',
+      post_image: null
     }
   },
   methods: {
     photoChange: function (event) {
-        this.post_image = event.target.files[0];
-        let reader = new FileReader();
-        reader.onload = () => {
-            this.$refs.preview.src = reader.result;
-            this.$refs.preview.style.display = "";
-            this.$refs.photoMeme.style.display = "none";
-        }
-        reader.readAsDataURL(this.post_image);
+      this.post_image = event.target.files[0];
+      let reader = new FileReader();
+      reader.onload = () => {
+        this.$refs.preview.src = reader.result;
+        this.$refs.preview.style.display = "";
+        this.$refs.photoMeme.style.display = "none";
+      }
+      reader.readAsDataURL(this.post_image);
     },
-
     submitFormModification() {
-        this.vModification$.$validate();
-        if(!this.vModification$.$error) {
-          return true;
-        } else {
-          this.error = '';
-          return false;
-        }
+      this.vModification$.$validate();
+      if(!this.vModification$.$error) {
+        return true;
+      } else {
+        this.error = '';
+        return false;
+      }
     },
     editPost: function () {
       if(this.submitFormModification()) {
@@ -171,16 +159,15 @@ export default {
       }
     },
     abandon: function () {
-        this.$router.push('/memes');
+      this.$router.push('/memes');
     }, 
   }
 }
-  
 </script>
 
 <style lang="scss" scoped>
 .formulaire {
-  border: 1px solid #bdc7d0;
+  @include border;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 }
 </style>
